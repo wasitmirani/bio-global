fea91ffb1458d7cba40526b44b5e1775814aa388
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2025-07-03 00:28:32 +0500

    update migrate

--- core/database/migrations/2024_06_10_113610_create_product_images_table.php
+++ core/database/migrations/2024_06_10_113610_create_product_images_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_images', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        // Schema::create('product_images', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
     }
 
     /**
